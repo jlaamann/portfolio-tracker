89adc66fbcbdfd86bbb7a7246ab55041f2bbcc1a
Johannes Laamann <jelaamann@gmail.com>
2025-05-05 23:57:26 +0300

    Fix background color not being uniform

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, useBreakpointValue, ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { Box, useBreakpointValue, ChakraProvider, ColorModeScript, useColorModeValue } from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import StockTicker from './components/StockTicker'
@@ -6,31 +6,49 @@ import HistoricalTicker from './components/HistoricalTicker'
 import Portfolio from './components/Portfolio'
 import theme from './theme'
 
-function App() {
+const AppContent = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
+  const bgColor = useColorModeValue('white', 'gray.800');
 
+  return (
+    <Box
+      display="flex"
+      minH="100vh"
+      w="100vw"
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      bottom={0}
+      overflow="auto"
+      bg={bgColor}
+    >
+      <Sidebar />
+      <Box
+        ml={isMobile ? 0 : "250px"}
+        p={4}
+        flex="1"
+        width="100%"
+      >
+        <Routes>
+          <Route path="/" element={<StockTicker />} />
+          <Route path="/historical" element={<HistoricalTicker />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+};
+
+function App() {
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Router>
-        <Box display="flex">
-          <Sidebar />
-          <Box
-            ml={isMobile ? 0 : "250px"}
-            p={4}
-            flex="1"
-            width="100%"
-          >
-            <Routes>
-              <Route path="/" element={<StockTicker />} />
-              <Route path="/historical" element={<HistoricalTicker />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-            </Routes>
-          </Box>
-        </Box>
+        <AppContent />
       </Router>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
